b3e29b81d0b8b12476db694929470e609d310f31
Shannon Ritenour <shannonritenour@gmail.com>
2021-03-08 12:51:37 -0600

    just needs advanced part of lab

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+
 let app = express();
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../public/index.html'));
 // });
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use((req, res, next) => {
+    console.log(req.originalUrl);
+    next();
+})
 
+// app.get('/', (req, res) => {
+//     res.send("What's happening?");
+// })
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(3000);
