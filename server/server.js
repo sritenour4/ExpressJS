b020239c0b5e5e2408d6c9e9794757cf462f5c94
Shannon Ritenour <shannonritenour@gmail.com>
2021-03-08 14:33:21 -0600

    finished

--- server/server.js
+++ server/server.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 let app = express();
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/index.html'));
-// });
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
     console.log(req.originalUrl);
     next();
+});
+
+app.use('/', (req, res, next) => {
+    let chirp = {username: req.body.username, message: req.body.message};
+    fs.appendFileSync('chirps.json', JSON.stringify(chirp), (err) => console.log(err));
+    next();
 })
 
-// app.get('/', (req, res) => {
-//     res.send("What's happening?");
-// })
+app.post('/formsubmissions', (req, res, next) => {
+    res.send(`Username: ${req.body.username} Message: ${req.body.message}`);
+});
 
-app.use(express.static(path.join(__dirname, '../public')));
-
+// tell express where to run the server
 app.listen(3000);
+
+
+
+// #3 from lab - use the get method on the home url, respond with the string
+// app.get('/', (req, res) => {
+//     res.send("Hello from the web server side...");
+// })
